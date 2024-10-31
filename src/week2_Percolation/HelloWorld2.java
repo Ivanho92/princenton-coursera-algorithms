d06bc6e52df273b21044be8ae95f4580f2d085e7
Ivan Rodrigues <51017707+Ivanho92@users.noreply.github.com>
2024-10-31 08:54:51 +0000

    test commit

--- src/week2_Percolation/HelloWorld2.java
+++ src/week2_Percolation/HelloWorld2.java
@@ -5,6 +5,6 @@ import edu.princeton.cs.algs4.StdOut;
 public class HelloWorld2 {
 
     public static void main(String[] args) {
-        StdOut.println("Hello, World 2!!!");
+        StdOut.println("Hello, World 2!!");
     }
 }
